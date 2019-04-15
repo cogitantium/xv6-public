0e48040f1f33f5f0109058e25930ac346dd02ad7
cogitantium <falke.cs@gmail.com>
2019-04-15 13:36:40 +0200

    added hostname to shell

--- sh.c
+++ sh.c
@@ -133,7 +133,7 @@ runcmd(struct cmd *cmd)
 int
 getcmd(char *buf, int nbuf)
 {
-  printf(2, "xv6-shell $ ");
+  printf(2, "xv6-shell user@thinktop $ ");
   memset(buf, 0, nbuf);
   gets(buf, nbuf);
   if(buf[0] == 0) // EOF
