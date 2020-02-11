82bfdb8c3db175af605e3f3f27ed88582f559de4
cogitantium <falke.cs@gmail.com>
2020-02-11 11:34:31 +0100

    [docker] Fix running xv6 on borked toolchain system

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# build image
+sudo docker run -it -v ~/aau/dat04/pss/xv6-public:/root/xv6-public -w /root/xv6-public/ xv6-build make xv6.img fs.img
+
+# fix permissions
+sudo chmod a+rwx xv6.img fs.img
+
+# run xv6.img
+make qemu-nox
