9dc716d0350074e63ab4359289b152a149b2e76e
cogitantium <falke.cs@gmail.com>
2020-02-24 14:37:36 +0100

    [docker] Add elaboration to build.sh for other users

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# build image
+# build image - FIXME: remember to change paths if you're not me
 sudo docker run -it -v ~/aau/dat04/pss/xv6-public:/root/xv6-public -w /root/xv6-public/ xv6-build make xv6.img fs.img
 
 # fix permissions
